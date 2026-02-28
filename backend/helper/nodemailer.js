3f888b0841d464acc0c84875cf889b5df1f7771e
Tanmay R Suradkar <tanmaysuradkar2008@gmail.com>
2026-02-28 16:10:56 +0530

    refactor: replace bcrypt with bcryptjs for the server issue &  improved compatibility

--- backend/helper/nodemailer.js
+++ backend/helper/nodemailer.js
@@ -1,5 +1,5 @@
 const UserModel = require("../models/user-model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 
 module.exports.sendEmail = async ({ email, emailType, user }) => {
