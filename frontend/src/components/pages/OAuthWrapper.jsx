5325456ff4e78e386496e0247607869a816aa8e2
Tanmay R Suradkar <tanmaysuradkar2008@gmail.com>
2026-03-01 21:55:51 +0530

    fix: correct user authentication dispatch in OAuthWrapper component

--- frontend/src/components/pages/OAuthWrapper.jsx
+++ frontend/src/components/pages/OAuthWrapper.jsx
@@ -4,20 +4,19 @@ import axios from "axios";
 import { useDispatch, useSelector } from 'react-redux';
 import {setUserInfo,clearUserInfo} from '../../redux/features/userInfo'
 const OAuthWrapper = () => {
-    const userInfo = useSelector((state)=> state.userInformation.value)
-    console.log(userInfo);
+    const userAuth = useSelector((state)=> state.userInformation.value)
+    console.log(userAuth);
     const dispatch = useDispatch()
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(true);
-    const setUserAuth = setUserAuth;
     useEffect(()=> {
         axios
             .get(`${import.meta.env.VITE_backendURL}auth/user`, {
                 withCredentials: true,
             })
             .then(async (res) => {
-                dispatch(setUserAuth( {email:res.data.userInfo.email, fullname: res.data.userInfo.fullname, _id: res.data.userInfo._id}))
+                dispatch(setUserInfo( {email:res.data.userInfo.email, fullname: res.data.userInfo.fullname, _id: res.data.userInfo._id}))
                 localStorage.removeItem("token");
                 console.log("user info:", userAuth);
                 localStorage.setItem("token", res.data.token);
@@ -25,8 +24,8 @@ const OAuthWrapper = () => {
                 console.log("user Auth info", res);
                 navigate("/shop");
             })
-            .catch(() => {
-                dispatch(setUserAuth())
+            .catch((err) => {
+                dispatch(clearUserInfo())
                 console.log("OAuth error:", err);
                 localStorage.removeItem("token");
                 navigate("/login");
