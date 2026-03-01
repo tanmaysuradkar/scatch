bae20ff6cd268dca824d0e4bd2b3f8f6cee47674
Tanmay R Suradkar <tanmaysuradkar2008@gmail.com>
2026-03-01 16:08:32 +0530

    fix: simplify error message for undefined MONGODB_URI and improve connection logging

--- backend/config/mongoose-connection.js
+++ backend/config/mongoose-connection.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
-const MONGODB_URI = process.env.MONGODB_URI;
 
+const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI is not defined in environment variables");
+  throw new Error("MONGODB_URI is not defined");
 }
+
 mongoose
-  .connect(`${MONGODB_URI}`)
-  .then(function () {
-    dbgr("MongoDB connected");
+  .connect(MONGODB_URI)
+  .then(() => {
+    dbgr("MongoDB connected:", mongoose.connection.name);
   })
-  .catch(function (err) {
-    dbgr("MongoDB connection error:",err);
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
   });
 
 module.exports = mongoose.connection;
