f41fa58d168e0fdbfcba40db170a5fd2463b7ded
Tanmay R Suradkar <tanmaysuradkar2008@gmail.com>
2026-05-21 16:09:38 +0530

    fix: restore MONGODB_URI check and use it for MongoDB connection

--- backend/config/mongoose-connection.js
+++ backend/config/mongoose-connection.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 
-// const MONGODB_URI = process.env.MONGODB_URI;
-// if (!MONGODB_URI) {
-//   throw new Error("MONGODB_URI is not defined");
-// }
+const MONGODB_URI = process.env.MONGODB_URI;
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined");
+}
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/scatch")
+  .connect(MONGODB_URI)
   .then(() => {
     dbgr("MongoDB connected:", mongoose.connection.name);
   })
   .catch((err) => {
-    console.error("MongoDB connection error:", MONGODB_URI);
+    console.error("MongoDB connection error:", err);
   });
 
 module.exports = mongoose.connection;
